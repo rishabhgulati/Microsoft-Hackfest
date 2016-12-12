1fc6f8b0b08e6856cf7f9e8571c12706f18ac715
Rishabh <Rishabh.Gulati@us.bosch.com>
2016-12-12 15:03:55 -0800

    basic chest pain dialog

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
@@ -24,21 +24,31 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 
 bot.dialog('/', [
-    function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+    function(session) {
+        builder.Prompts.text(session, "Hello... What type of pain are you having?");
     },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?");
+    function(session, results) {
+        //todo: trigger LUIS to and trigger the dialog based on that
+        if (results.response.includes("chest")) {
+            session.userData.pain = "Chest Pain";
+            builder.Prompts.choice(session, "How severe?", ["Mild", "Sharp", "Severe"]);
+        } else {
+            builder.Prompts.text(session, "I don't know about this. Sorry");
+        }
     },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name +
-                     " you've been programming for " + session.userData.coding +
-                     " years and use " + session.userData.language + ".");
+    function(session, results) {
+        session.userData.painLevel = results.response.entity;
+        console.log("pain level " + session.userData.painLevel);
+        switch (session.userData.painLevel) {
+            case "Mild":
+            case "Sharp":
+                builder.Prompts.text(session, "Lets continue further");
+                break;
+            case "Severe":
+                builder.Prompts.text(session, "Pain is severe, connecting to a Professional");
+                break;
+            default:
+                builder.Prompts.text(session, "Unknown state");
+        }
     }
 ]);
