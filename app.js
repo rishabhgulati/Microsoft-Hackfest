d96f2496899dd898910e7d7f5c62230cacdcdd53
Jon Francis <jmtf629@gmail.com>
2016-12-13 17:32:36 -0800

    Integration

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
+var builder = require('botbuilder');
 var restify = require('restify')
-,   builder = require('botbuilder')
 ,   drbe = require('./packages/drbe/drbe.js');
-
-
-//=========================================================
-// Bot Setup
-//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
@@ -21,38 +16,64 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
+//const LuisModelUrl = process.env.LUIS_URL;
 
-var emergencies = ["Health", "Crime", "Catastrophe"];
+// Main dialog with LUIS
+//var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var recognizer = new builder.LuisRecognizer('https://iswudev.azure-api.net/luis/v2.0/apps/98eead94-8470-4337-9280-5bb7d5fb8502?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true');
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+    .matches('sendCall', [
+        function (session, args, next) {
 
-bot.dialog('/', [
-    //welcome the user, ask the emergency
-    function(session) {
-        session.send("Hello");
-        builder.Prompts.choice(session, "What's the emergency?", emergencies);
-    },
-    //work with selected emergency
-    function(session, results) {
-        session.userData.emergency = results.response.entity;
-        switch (session.userData.emergency) {
-            case emergencies[0]:
-                session.send(emergencies[0]);
-                session.replaceDialog('/Health');
-                break;
-            case emergencies[1]:
-                session.send(emergencies[1]);
-                break;
-            case emergencies[2]:
-                session.send(emergencies[2]);
-                break;
-            default:
-                //no default case required as the framework handles invalid inputs
-                //and prompts the user to enter a valid input
+            var contact = builder.EntityRecognizer.findEntity(args.entities, 'emergency contact');
+            var name = builder.EntityRecognizer.findEntity(args.entities, 'name');
+            var encyclopedia = builder.EntityRecognizer.findEntity(args.entities, 'encyclopedia');
+
+            var msg = "";
+
+            if (contact) {
+                msg += "I think your contact is: "+contact.entity+"\n";
+            }
+            if (name) {
+                msg += "I think your name is: "+name.entity+"\n";
+            }
+            if (encyclopedia) {
+                msg += "I think your encyclopedia is: "+encyclopedia.entity+"\n";
+            }
+
+
+            session.send('sendCall triggered: \'%s\'\n\n%s', session.message.text,msg);
         }
-    }
-]);
+    ])
+    .matches('diagnose', [
+        function (session, args, next) {
+
+            // try extracting entities
+            var intensity = builder.EntityRecognizer.findEntity(args.entities, 'intensity');
+            var contact = builder.EntityRecognizer.findEntity(args.entities, 'emergency contact');
+            var location = builder.EntityRecognizer.findEntity(args.entities, 'location');
+
+            var msg = "";
+
+            if (intensity) {
+                msg += "I think your intensity is: "+intensity.entity+"\n";
+            }
+            if (contact) {
+                msg += "I think your contact is: "+contact.entity+"\n";
+            }
+            if (location) {
+                msg += "I think your location is: "+location.entity+"\n";
+            }
+
+
+            session.send('diagnos triggered: \'%s\'\n\n%s', session.message.text,msg);
+        }
+    ])
+    .onDefault((session) => {
+        session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+    });
+
+bot.dialog('/', intents);
 
 //health conversation
 bot.dialog('/Health', [
