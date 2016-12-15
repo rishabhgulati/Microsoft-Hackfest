06080f6c06007e422f945660d6da015e898f7a5e
Jon Francis <jmtf629@gmail.com>
2016-12-15 09:32:49 -0800

    Mounting LUIS on root endpoint

--- dialog.luis.js
+++ dialog.luis.js
@@ -2,7 +2,7 @@ exports.luis = function(builder){
 
     // Main dialog with LUIS
     //
-    var LuisModeUrl = process.env.LUIS_URL;
+    var LuisModeUrl = process.env.LUIS_URL || 'https://iswudev.azure-api.net/luis/v2.0/apps/8b5b16e1-6cdb-4b1c-ac30-62c4d499c6cd?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true';
     var recognizer = new builder.LuisRecognizer(LuisModeUrl);
     //var recognizer = new builder.LuisRecognizer('https://iswudev.azure-api.net/luis/v2.0/apps/98eead94-8470-4337-9280-5bb7d5fb8502?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true');
     //var recognizer = new builder.LuisRecognizer('https://iswudev.azure-api.net/luis/v2.0/apps/8b5b16e1-6cdb-4b1c-ac30-62c4d499c6cd?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true');
