de09b0d14ca781657f599694c0ea6cfbfe83bcb1
Rishabh <Rishabh.Gulati@us.bosch.com>
2016-12-15 14:53:54 -0800

    handling intensity to trigger places api

--- dialog.health.js
+++ dialog.health.js
@@ -1,88 +1,56 @@
 //health conversation
 
-exports.healthDialog = function (bot, builder, bandDataHandler){
+exports.healthDialog = function(bot, builder, bandDataHandler) {
 
-  //parse google places api response to create cards, where data is a JSON object
-  var getCards = function(data, session, bot, builder) {
-      var noOfResults = data.results.length;
+    //var intents = require("./dialog.luis.origin.js").luis(builder);
+    var intents = require("./packages/luis/bot-frida/dialog.luis.main.js");
+    intents.setBandDataHandler(bandDataHandler);
+    bot.dialog('/', intents);
 
-      if (noOfResults == 0) {
-          console.log("no results available");
-          return null;
-      } else {
-          //show a maximum of 3 cards
-          var noOfCards = (noOfResults < 3) ? noOfResults : 3;
-          var cards = [];
-          for (var i = 0; i < noOfCards; i++) {
-              var result = data.results[i];
-              cards[i] = new builder.HeroCard(session)
-                  .title(result.name)
-                  .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
-                  .images([
-                      builder.CardImage.create(session, result.icon)
-                  ])
-                  .buttons([
-                      builder.CardAction.openUrl(session, 'https://google.com/', 'Learn More')
-                  ]);
-          }
+    var dialog = bot.dialog('/health', [
+        function(session) {
+            builder.Prompts.text(session, "What's your condition?");
+        },
+        //figure out the type of emergency. Later use LUIS to get the emergency
+        function(session, results) {
+            if (results.response.includes("chest")) {
+                session.userData.painType = "chestpain";
+                builder.Prompts.choice(session, "How severe is the pain?", ["Mild", "Sharp", "Severe"]);
+            } else if (results.response.includes("headache")) {
+                session.userData.painType = "headache";
+                builder.Prompts.choice(session, "How severe is the pain?", ["Mild", "Sharp", "Severe"]);
+            } else {
+                builder.Prompts.text(session, "I can only help diagnosing chestpain & headache");
+            }
+        },
+        function(session, results) {
+            session.userData.painLevel = results.response.entity;
+            switch (session.userData.painLevel) {
+                case "Mild":
+                    session.send("Ping --> Mild case");
+                    break;
+                case "Sharp":
+                    //retrieve heartrate
+                    if (typeof sensor === "undefined") {
+                        session.send("Unable to fetch heartrate");
+                    } else {
+                        session.send("Your current heartrate is " + sensor.getLastHeartRate());
+                    }
+                    break;
+                case "Severe":
 
-          var reply = new builder.Message(session)
-              .attachmentLayout(builder.AttachmentLayout.carousel)
-              .attachments(cards);
+                    break;
+                default:
+                    session.replaceDialog("/");
+                    //no default case required as the framework handles invalid inputs
+                    //and prompts the user to enter a valid input
+            }
+        },
+        function(session) {
 
-          session.send(reply);
-      }
-  }
+        }
+    ]);
 
-  //var intents = require("./dialog.luis.origin.js").luis(builder);
-  var intents = require("./packages/luis/bot-frida/dialog.luis.main.js");
-  bot.dialog('/',intents);
-
-  var dialog = bot.dialog('/health', [
-      function(session) {
-          builder.Prompts.text(session, "What's your condition?");
-      },
-      //figure out the type of emergency. Later use LUIS to get the emergency
-      function(session, results) {
-          if (results.response.includes("chest")) {
-              session.userData.painType = "chestpain";
-              builder.Prompts.choice(session, "How severe is the pain?", ["Mild", "Sharp", "Severe"]);
-          }
-          else if (results.response.includes("headache")) {
-              session.userData.painType = "headache";
-              builder.Prompts.choice(session, "How severe is the pain?", ["Mild", "Sharp", "Severe"]);
-          } else {
-              builder.Prompts.text(session, "I can only help diagnosing chestpain & headache");
-          }
-      },
-      function(session, results) {
-          session.userData.painLevel = results.response.entity;
-          switch (session.userData.painLevel) {
-              case "Mild":
-                  session.send("Ping --> Mild case");
-                  break;
-              case "Sharp":
-                  //retrieve heartrate
-                  if (typeof sensor === "undefined") {
-                    session.send("Unable to fetch heartrate");
-                  } else {
-                    session.send("Your current heartrate is " + sensor.getLastHeartRate());
-                  }
-                  break;
-              case "Severe":
-
-                  break;
-              default:
-                  session.replaceDialog("/");
-                  //no default case required as the framework handles invalid inputs
-                  //and prompts the user to enter a valid input
-          }
-      },
-      function(session){
-
-      }
-  ]);
-
-  return dialog;
+    return dialog;
 
 };
--- packages/luis/bot-frida/dialog.luis.main.js
+++ packages/luis/bot-frida/dialog.luis.main.js
@@ -1,22 +1,61 @@
 "use strict"
 const builder = require('botbuilder');
 var places = require('./../../../libs/places/places.js');
-var Bing = require('node-bing-api')({accKey: process.env.BING_SEARCH});
+var Bing = require('node-bing-api')({
+    accKey: process.env.BING_SEARCH
+});
 
-const LuisModeUrl = /*process.env.LUIS_URL ||*/'https://iswudev.azure-api.net/luis/v2.0/apps/c4af3be6-61b2-4c02-8b12-6a1f7f10eec8?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true';
+const LuisModeUrl = /*process.env.LUIS_URL ||*/ 'https://iswudev.azure-api.net/luis/v2.0/apps/c4af3be6-61b2-4c02-8b12-6a1f7f10eec8?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true';
 const recognizer = new builder.LuisRecognizer(LuisModeUrl);
 //const recognizer = new builder.LuisRecognizer('https://iswudev.azure-api.net/luis/v2.0/apps/98eead94-8470-4337-9280-5bb7d5fb8502?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true');
 //const recognizer = new builder.LuisRecognizer('https://iswudev.azure-api.net/luis/v2.0/apps/8b5b16e1-6cdb-4b1c-ac30-62c4d499c6cd?subscription-key=c2cd164e833947fbb41ae9a3d9886a1f&verbose=true');
-module.exports = new builder.IntentDialog({ recognizers: [recognizer] })
+
+//parse google places api response to create cards, where data is a JSON object
+var getCards = function(data, session, bot, builder) {
+    var noOfResults = data.results.length;
+
+    if (noOfResults == 0) {
+        console.log("no results available");
+        return null;
+    } else {
+        //show a maximum of 3 cards
+        var noOfCards = (noOfResults < 3) ? noOfResults : 3;
+        var cards = [];
+        for (var i = 0; i < noOfCards; i++) {
+            var result = data.results[i];
+            cards[i] = new builder.HeroCard(session)
+                .title(result.name)
+                .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+                .images([
+                    builder.CardImage.create(session, result.icon)
+                ])
+                .buttons([
+                    builder.CardAction.openUrl(session, 'https://google.com/', 'Learn More')
+                ]);
+        }
+
+        var reply = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.carousel)
+            .attachments(cards);
+
+        session.send(reply);
+    }
+}
+
+module.exports = new builder.IntentDialog({
+        recognizers: [recognizer]
+    })
     .matches('diagnose', [
         (session, args, next) => {
-            var fetchArgs = (args)? args : null;
+            var fetchArgs = (args) ? args : null;
             console.log(fetchArgs);
 
             session.privateConversationData.args = args;
             let userState = session.privateConversationData.userState = builder.EntityRecognizer.findEntity(args.entities, 'UserState');
             if (userState) {
-                next({ response: userState });
+                next({
+                    response: userState
+                });
             } else {
                 builder.Prompts.text(session, 'What is your current condition?');
             }
@@ -31,72 +70,57 @@ module.exports = new builder.IntentDialog({ recognizers: [recognizer] })
                 session.endConversation("I didn't get a response. Exiting conversation.");
             } else {
                 // retrieve the intensity
-                builder.Prompts.number(session, 'What is the intensity on a scale of 1-10, with 10 being worst.');
+                //builder.Prompts.number(session, 'What is the intensity on a scale of 1-10, with 10 being worst.');
+                builder.Prompts.choice(session, "How severe is the pain?", ["Mild", "Sharp", "Severe"]);
             }
         },
         (session, results) => {
-            let intensity = results.response;
-            if(!intensity) {
-                // no value provided. exit
-                session.endConversation("I didn't receive a response");
-            } else if(intensity == 'high') {
-                // determine location
-                builder.Prompts.text(session, 'What is your location?');
-                //fetch GPS location?
-            } else if((intensity == 'low') || (intensity == 'med')) {
-
-                var heartRate = bandDataHandler.getLastHeartRate();
-                if (typeof heartRate === 'undefined' || heartRate == 0) {
-                    session.send("Can't detect heart rate");
-                    //suggest doctors/hospitals nearby
-                    places.getPlaces(bandDataHandler.getLatitude(), bandDataHandler.getLongitude(), function(data) {
-                        getCards(data, session, bot, builder);
-                    });
-                    session.endDialog();
-                    //TODO prompt question to user
-                } else {
-                    if (heartRate > 60 && heartRate < 100) {
-                        session.send("Heartrate seems to be normal");
+            switch (results.response.entity) {
+                case "Mild":
+                case "Sharp":
+                    if (bandDataHandler.getLatitude() == 0 || bandDataHandler.getLongitude() == 0) {
+                        session.send("Can't detect lat/lng");
+                    } else {
                         //suggest doctors/hospitals nearby
                         places.getPlaces(bandDataHandler.getLatitude(), bandDataHandler.getLongitude(), function(data) {
                             getCards(data, session, bot, builder);
                         });
                         session.endDialog();
-                    } else {
-                        session.send("Calling 911, heartrate abnormal " + heartRate);
-                        session.endDialog();
                     }
-                }
-
-                session.endConversation("(Severity --> low/med) You're not dead yet. (keyword: yet)");
-            } else {
-                session.endConversation("You're not dead yet. (keyword: yet)");
+                    break;
+                case "Severe":
+                    session.send("This could be a heart attack, Calling 911");
+                    session.endDialog();
+                    break;
+                default:
+                    //no default case required as the framework handles invalid inputs
+                    //and prompts the user to enter a valid input
             }
         },
         (session, results) => {
             let location = results.response;
-            if(location === 'Pittsburgh') {
+            if (location === 'Pittsburgh') {
                 session.endConversation("I'm going to call your wife.");
             } else {
                 session.endConversation("I'm going to call an ambulance");
             }
         }
     ])
-    .matches('search',[
-      (session, args, next) => {
-        session.send("(\'search\' activation)");
-        Bing.composite(JSON.stringify(session.message.text), {
-          top: 5
-        }, (error, res, body) => {
-          session.send("Here are some search results for "+JSON.stringify(session.message.text)+": %s", JSON.stringify(body.news));
-        })
-      }
+    .matches('search', [
+        (session, args, next) => {
+            session.send("(\'search\' activation)");
+            Bing.composite(JSON.stringify(session.message.text), {
+                top: 5
+            }, (error, res, body) => {
+                session.send("Here are some search results for " + JSON.stringify(session.message.text) + ": %s", JSON.stringify(body.news));
+            })
+        }
     ])
     .onDefault([
-      (session, args, next) => {
-        var fetchArgs = (args)? args : null;
-        console.log(fetchArgs);
+        (session, args, next) => {
+            var fetchArgs = (args) ? args : null;
+            console.log(fetchArgs);
 
-        session.endConversation("Triggered \'onDefault\' -- Done");
-      }
+            session.endConversation("Triggered \'onDefault\' -- Done");
+        }
     ]);
